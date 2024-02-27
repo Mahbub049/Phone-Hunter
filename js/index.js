930541d462cfbf7e4443b7aea706ec98ce683c33
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-02-27 23:28:35 +0600

    phone hunter functionality added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const dataLoad = async(searchText, isShow) => {
+const dataLoad = async(searchText='samsung', isShow) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await  response.json();
     const phones = data.data;
@@ -33,7 +33,7 @@ const displayPhones = (phones, isShow) =>{
             <p class="px-9">There are many variations of passages of available, but the majority have suffered</p>
             <h2 class="text-2xl font-bold pt-2 pb-6">$999</h2>
             <div class="card-actions">
-                <button class="btn bg-[#0D6EFD] text-xl font-semibold text-white">Show Details</button>
+                <button onclick="showDetailsModal('${phone.slug}')" class="btn bg-[#0D6EFD] text-xl font-semibold text-white">Show Details</button>
             </div>
             </div>
        `
@@ -64,7 +64,33 @@ const toggle = (isLoading) => {
 
 const showAllData = () => {
     searchPhone(true);
+}
 
+const showDetailsModal = async(id) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    console.log(phone);
+    my_modal_5.showModal();
+
+    const modal = document.getElementById('modalContainer');
+    modal.innerHTML = `
+        <div class="flex justify-center py-6">
+        <img class="w-1/4" src="${phone.image}" alt="">
+    </div>
+    <div class="space-y-2">
+        <h3 class="text-3xl font-bold">${phone.name}</h3>
+        <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        <p class="text-xl "><span class="text-xl font-semibold">Storage : </span>${phone?.mainFeatures?.storage}</p>
+        <p class="text-xl "><span class="text-xl font-semibold">Display Size : </span>${phone?.mainFeatures?.displaySize}</p>
+        <p class="text-xl "><span class="text-xl font-semibold">Chipset : </span>${phone?.mainFeatures?.chipSet}</p>
+        <p class="text-xl "><span class="text-xl font-semibold">Memory : </span>${phone?.mainFeatures?.memory}</p>
+        <p class="text-xl "><span class="text-xl font-semibold">Slug : </span>${phone?.slug}</p>
+        <p class="text-xl "><span class="text-xl font-semibold">Release Date : </span>${phone?.releaseDate}</p>
+        <p class="text-xl "><span class="text-xl font-semibold">Brand : </span>${phone?.brand}</p>
+        <p class="text-xl "><span class="text-xl font-semibold">GPS : </span>${phone?.others?.GPS}</p>
+    </div>
+    `
 }
 
 dataLoad();
