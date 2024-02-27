413d3bf42563ad83afcc734e56ee1745cb4b66bf
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-02-27 20:13:47 +0600

    dynamically image and name added

--- js/index.js
+++ js/index.js
@@ -2,7 +2,32 @@ const dataLoad = async() => {
     const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await  response.json();
     const phones = data.data;
-    console.log(phones)
+    displayPhones(phones);
 }
 
-dataLoad()
+const displayPhones = (phones) =>{
+    const phoneContainer = document.getElementById('phoneContainer');
+    console.log(phones);
+    phones.forEach(phone => {
+       const phoneDiv = document.createElement('div');
+       phoneDiv.classList.add = `card w-96 bg-base-100 shadow-xl border-[#CFCFCF]`
+
+       phoneDiv.innerHTML = `
+            <figure class="px-10 pt-10">
+            <img src="${phone.image}" alt="Shoes" class="rounded-xl w-1/2 mx-auto" />
+            </figure>
+            <div class="card-body items-center text-center">
+            <h2 class="card-title text-2xl font-bold py-5">${phone.phone_name}</h2>
+            <p class="px-9">There are many variations of passages of available, but the majority have suffered</p>
+            <h2 class="text-2xl font-bold pt-2 pb-6">$999</h2>
+            <div class="card-actions">
+                <button class="btn bg-[#0D6EFD] text-xl font-semibold text-white">Show Details</button>
+            </div>
+            </div>
+       `
+
+       phoneContainer.appendChild(phoneDiv)
+    });
+}
+
+dataLoad();
